499df15b6d8ce3e953bd4d8d551d1cec3227f46f
dmc <david.mark.clements@gmail.com>
2025-01-30 20:48:24 +0100

    conditionally load script-linker/runtime in preload

--- preload.js
+++ preload.js
@@ -43,6 +43,76 @@ module.exports = (state) => {
     })
   }
 
+  const { warn } = console
+  console.warn = (msg, ...args) => {
+    // if (/Insecure Content-Security-Policy/.test(msg)) return.
+    warn.call(console, msg, ...args)
+  }
+
+  if (location.hostname === 'localhost' && location.port) {
+    const gunk = require('pear-api/gunk')
+    const runtime = require('script-linker/runtime')
+
+    // platform runtime:
+    const pltsl = runtime({
+      builtins: gunk.builtins,
+      map: gunk.platform.map,
+      mapImport: gunk.platform.mapImport,
+      symbol: gunk.platform.symbol,
+      protocol: gunk.platform.protocol,
+      getSync (url) {
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', url, false)
+        xhr.send(null)
+        return xhr.responseText
+      },
+      resolveSync (req, dirname, { isImport }) {
+        const xhr = new XMLHttpRequest()
+        const type = isImport ? 'esm' : 'cjs'
+        const url = `${dirname}/~${req}+platform-resolve+${type}`
+        xhr.open('GET', url, false)
+        xhr.send(null)
+        return xhr.responseText
+      }
+    })
+
+    // app runtime:
+    const appsl = runtime({
+      builtins: gunk.builtins,
+      map: gunk.app.map,
+      mapImport: gunk.app.mapImport,
+      symbol: gunk.app.symbol,
+      protocol: gunk.app.protocol,
+      getSync (url) {
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', url, false)
+        xhr.send(null)
+        return xhr.responseText
+      },
+      resolveSync (req, dirname, { isImport }) {
+        const xhr = new XMLHttpRequest()
+        const type = isImport ? 'esm' : 'cjs'
+        const url = `${dirname}/~${req}+resolve+${type}`
+        xhr.open('GET', url, false)
+        xhr.send(null)
+        if (xhr.status !== 200) throw new Error(`${xhr.status} ${xhr.responseText}`)
+        return xhr.responseText
+      }
+    })
+
+    async function warm () {
+      for await (const { batch, protocol } of Pear[Pear.constructor.UI].warming()) {
+        let sl = null
+        if (protocol === 'pear' || protocol === 'holepunch') sl = pltsl
+        if (protocol === 'app') sl = appsl
+        if (sl === null) continue
+        for (const { filename, source } of batch) sl.sources.set(filename, source)
+      }
+    }
+
+    if (Pear.config.isDecal === false) warm().catch(console.error)
+  }
+
   function descopeGlobals () {
     delete global.require
     delete global.module
@@ -50,21 +120,7 @@ module.exports = (state) => {
     delete global.__filename
   }
 
-  const { warn } = console
-  console.warn = (msg, ...args) => {
-    if (/Insecure Content-Security-Policy/.test(msg)) return
-    warn.call(console, msg, ...args)
-  }
-
-  if (Pear.config.options.gui?.preload) {
-    const run = require('node-bare-bundle')
-    const key = Pear.config.options.gui.preload
-    Pear.get(key, { bundle: true }).then((preload) => {
-      run(preload, { mount: '/', entrypoint: key })
-    }, console.error).finally(descopeGlobals)
-  } else {
-    descopeGlobals()
-  }
+  descopeGlobals()
 
   customElements.define('pear-ctrl', class extends HTMLElement {
     #onfocus = null
