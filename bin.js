a91c22ba1a9d4bb3fd7bbfa88bd06cfa9aa4cc81
dmc <david.mark.clements@gmail.com>
2025-02-06 22:30:26 +0100

    bootstrap output, use ipc dump, stats

--- bin.js
+++ bin.js
@@ -2,27 +2,43 @@
 /* global Pear */
 
 if (!global.Pear && global.process) {
-  const { status } = require('child_process').spawnSync('pear', process.argv.slice(1), { stdio: 'inherit' })
+  const { status } = require('child_process').spawnSync('pear', process.argv.slice(1), { stdio: 'inherit', shell: true })
   process.exit(status)
 }
 
 const path = require('path')
-const { runtimes } = global.Pear.config.entrypoint.startsWith('/node_modules/.bin/')
-  ? require('../pear-electron/package.json').pear
-  : require('./package.json').pear
+const inDotBin = global.Pear.config.entrypoint.startsWith('/node_modules/.bin/')
+const { runtimes } = inDotBin ? require('../pear-electron/package.json').pear : require('./package.json').pear
 const IPC = require('pear-ipc')
 const { encode } = require('hypercore-id-encoding')
 const { PLATFORM_LOCK, SOCKET_PATH, CONNECT_TIMEOUT } = require('pear-api/constants')
 const tryboot = require('pear-api/tryboot')
 const link = require('pear-link')()
+const byteSize = require('tiny-byte-size')
+const { outputter, ansi } = require('pear-api/terminal')
+
+const output = outputter('dump', {
+  dumping: ({ link, dir, list }) => list > -1 ? '' : `\n${ansi.pear} Bootstrapping pear-electron runtimes from peers\n\nfrom: ${link}\ninto: ${dir}\n`,
+  file: ({ key, value }) => `${key}${value ? '\n' + value : ''}`,
+  complete: () => '\x1b[1A\nBootstrap complete\n',
+  stats ({ upload, download, peers }) {
+    const dl = download.total + download.speed === 0 ? '' : `[⬇ ${byteSize(download.total)} - ${byteSize(download.speed)}/s ]`
+    const ul = upload.total + upload.speed === 0 ? '' : `[⬆ ${byteSize(upload.total)} - ${byteSize(upload.speed)}/s ]`
+    return {
+      output: 'status',
+      message: `${dl} ${ul} [ Peers: ${peers} ]`
+    }
+  },
+  error: (err) => `Bootstrap Failure (code: ${err.code || 'none'}) ${err.stack}`
+})
 
 async function pearElectron () {
   const { protocol, drive } = link(runtimes)
-
+  const root = new URL(Pear.config.applink).pathname
   const opts = {
     id: Pear.pid,
     link: protocol + '//' + encode(drive.key) + '/by-arch/',
-    dir: path.join(new URL(Pear.config.applink).pathname, 'node_modules', 'pear-electron'),
+    dir: inDotBin ? path.join(root, 'node_modules', 'pear-electron') : root,
     // checkout: drive.length,
     force: true
 
@@ -34,8 +50,7 @@ async function pearElectron () {
     connect: tryboot
   })
   await ipc.ready()
-  const stream = ipc.dump(opts)
-  for await (const output of stream) console.log(output)
+  await output(false, ipc.dump(opts))
   await ipc.close()
 }
 
