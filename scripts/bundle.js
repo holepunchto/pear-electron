c758a30968ec3758cca4cbab9313f91ecf04000d
dmc <david.mark.clements@gmail.com>
2025-02-21 14:12:07 +0100

    multi target bundle

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,32 +1,16 @@
 'use strict'
-const Bundle = require('bare-bundle')
+const pack = require('bare-pack')
 const Localdrive = require('localdrive')
 const DriveBundler = require('drive-bundler')
 const path = require('path')
-const fs = require('fs')
 const dirname = global.Pear?.config.applink ? new URL(global.Pear.config.applink + '/').pathname : path.join(__dirname, '/..')
+
 async function bundle () {
   const drive = new Localdrive(dirname)
-  const b = new Bundle()
-
-  const cache = {}
-  const res = {}
-
-  const { entrypoint, resolutions, sources } = await DriveBundler.bundle(drive, { cache, cwd: dirname, entrypoint: '/boot.js', absoluteFiles: false })
-
-  for (const [key, map] of Object.entries(resolutions)) {
-    res[key] = map
-  }
-
-  for (const [key, source] of Object.entries(sources)) {
-    cache[key] = true
-
-    b.write(key, source)
-  }
-
-  b.main = entrypoint
-  b.resolutions = res
-  await fs.promises.writeFile(path.join(dirname, 'boot.bundle'), b.toBuffer())
+  const { sources, resolutions } = await DriveBundler.bundle(drive, { cwd: dirname, entrypoint: '/boot.js', mount: 'pear://ui', absoluteFiles: false, prebuilds: false })
+  const target = ['darwin-arm64', 'darwin-x64', 'linux-arm64', 'linux-x64', 'win32-x64']
+  const b = await pack(new URL('pear://ui/boot.js'), { resolutions, target }, (url) => sources[url.href])
+  await drive.put('/boot.bundle', b.toBuffer())
   console.log('boot.bundle generated')
 }
 
