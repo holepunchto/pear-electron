5450a73d29fe25b9aca428b44db399de49b63668
dmc <david.mark.clements@gmail.com>
2025-03-07 13:58:31 +0100

    use pear-pack

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,9 +1,5 @@
 'use strict'
-const sodium = require('sodium-native')
-const pack = require('bare-pack-drive')
-const unpack = require('bare-unpack')
-const lex = require('bare-module-lexer')
-const traverse = require('bare-module-traverse')
+const pack = require('pear-pack')
 const Localdrive = require('localdrive')
 
 async function bundle () {
@@ -15,58 +11,10 @@ async function bundle () {
     'path', 'child_process', 'repl', 'url', 'tty', 'module', 'process', 'timers', 'inspector',
     'rocksdb-native'
   ]
-  const bundle = await pack(drive, '/boot.js', { resolve, target, builtins })
-  const rebundle = await unpack(bundle, { addons: true, files: false }, async (key) => {
-    const extIx = key.lastIndexOf('.')
-    if (extIx === -1) return key
-    const extname = key.slice(extIx)
-    if (extname !== '.node' && extname !== '.bare') return key
-    const hash = Buffer.allocUnsafe(32)
-    const addon = await drive.get(key)
-    sodium.crypto_generichash(hash, addon)
-    const prebuilds = key.slice(key.indexOf('/prebuilds/'))
-    const prebuild = prebuilds.slice(0, prebuilds.lastIndexOf('/') + 1) + hash.toString('hex') + extname
-    await drive.put(prebuild, addon)
-    return '/..' + prebuild
-  })
-  await drive.put('/boot.bundle', rebundle.toBuffer())
-  console.log('boot.bundle generated')
+  const { bundle, prebuilds } = await pack(drive, { target, builtins })
+  for (const [prebuild, addon] of prebuilds) await drive.put(prebuild, addon)
+  await drive.put('/boot.bundle', bundle)
+  console.log('boot.bundle & prebuilds generated')
 }
 
 bundle().catch(console.error)
-
-function resolve (entry, parentURL, opts = {}) {
-  let extensions
-  let conditions = opts.target.map((host) => ['node', 'bare', ...host.split('-')])
-
-  if (entry.type & lex.constants.ADDON) {
-    extensions = ['.node', '.bare']
-    conditions = conditions.map((conditions) => ['addon', ...conditions])
-
-    return traverse.resolve.addon(entry.specifier || '.', parentURL, {
-      extensions,
-      conditions,
-      hosts: opts.target,
-      linked: false,
-      ...opts
-    })
-  }
-
-  if (entry.type & lex.constants.ASSET) {
-    conditions = conditions.map((conditions) => ['asset', ...conditions])
-  } else {
-    extensions = ['.js', '.cjs', '.mjs', '.json', '.node', '.bare']
-
-    if (entry.type & lex.constants.REQUIRE) {
-      conditions = conditions.map((conditions) => ['require', ...conditions])
-    } else if (entry.type & lex.constants.IMPORT) {
-      conditions = conditions.map((conditions) => ['import', ...conditions])
-    }
-  }
-
-  return traverse.resolve.module(entry.specifier, parentURL, {
-    extensions,
-    conditions,
-    ...opts
-  })
-}
