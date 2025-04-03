f94d4794918112eb0032adbaaf5b1ba3a3893bde
dmc <david.mark.clements@gmail.com>
2025-04-03 20:45:48 +0200

    runtimes -> bootstrap, pass bootstrap link straight to dump, tidy deps

--- bin.js
+++ bin.js
@@ -7,22 +7,17 @@ if (!global.Pear && global.process) {
 const path = require('bare-path')
 const installed = global.Pear.config.entrypoint.startsWith('/node_modules/.bin/')
 const pkg = installed ? require('../pear-electron/package.json') : require('./package.json')
-const { runtimes } = pkg.pear
-const { encode } = require('hypercore-id-encoding')
-const link = require('pear-link')()
 const { isTTY } = require('pear-api/terminal')
 const bootstrap = installed ? require('../pear-electron/bootstrap') : require('./bootstrap')
 
 async function pearElectron () {
-  const { protocol, drive } = link(runtimes)
   const { pathname } = new URL(global.Pear.config.applink)
   const opts = {
     id: global.Pear.pid,
     dir: installed ? path.join(pathname, 'node_modules', 'pear-electron') : pathname,
-    link: protocol + '//' + encode(drive.key),
+    link: pkg.pear.bootstrap,
     only: installed ? ['/by-arch', '/prebuilds'] : ['/by-arch'],
     json: !isTTY,
-    // checkout: drive.length,
     force: true
 
   }
