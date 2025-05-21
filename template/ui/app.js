4f7048178cb46205cab1d25a5b06d3075f280766
dmc <david.mark.clements@gmail.com>
2025-05-21 20:50:22 +0200

    align template

--- template/ui/app.js
+++ template/ui/app.js
@@ -1,2 +1,6 @@
 /** @typedef {import('pear-interface')} */ /* global Pear */
+import ui from 'pear-electron'
+
 document.querySelector('h1').addEventListener('click', (e) => { e.target.innerHTML = '🍐' })
+
+console.log(await ui.dimensions()) // log app dimensions
