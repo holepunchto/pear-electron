510d36a25ed56635eceddca7453f55fe5f65445c
dmc <david.mark.clements@gmail.com>
2025-05-21 20:05:57 +0200

    no need for bin

--- bin.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pear
-/* global Pear */
-if (!global.Pear && global.process) {
-  const { status } = require('child_process').spawnSync('pear', process.argv.slice(1), { stdio: 'inherit', shell: true })
-  process.exit(status)
-}
-if (Pear.pipe === undefined) throw new Error('Incompatible with current Pear version (must be v2+)')
-if (Pear.pipe === null) throw new Error('This is a pre script: must have Pear.pipe')
-require('./pre')
