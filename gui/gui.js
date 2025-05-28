73be1cf800ac484ed5ff3acf2e95c6d7643637ba
Geordan Gesink <165498764+geordangesink@users.noreply.github.com>
2025-05-28 11:30:16 +0200

    fix/before-quit (#46)

--- gui/gui.js
+++ gui/gui.js
@@ -614,6 +614,11 @@ class App {
           }
         }
       })
+
+      electron.app.once('before-quit', () => {
+        ctrl.quitting = true
+      })
+
       this.id = ctrl.id
       await this.starting
     } catch (err) {
@@ -958,7 +963,7 @@ class GuiCtrl {
 
     const closeListener = (e) => {
       e.preventDefault()
-      if (this.win.closeHides) return
+      if (this.win.closeHides && this.quitting === false) return
       if (this.unload) {
         this.unload({ type: 'close' })
       }
@@ -972,6 +977,7 @@ class GuiCtrl {
   }
 
   completeUnload (action) {
+    this.quitting = true
     this.unloaded()
     if (action.type === 'close') this.close()
   }
