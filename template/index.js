f61c26188441fe9e999c4bde237ffa72fdd4283f
dmc <david.mark.clements@gmail.com>
2025-07-31 22:08:25 +0200

    fixes

--- template/index.js
+++ template/index.js
@@ -6,7 +6,7 @@ Pear.updates((update) => {
   console.log('Application update available:', update)
 })
 
-const bridge = new Bridge({ waypoint: '/ui/index.html' })
+const bridge = new Bridge({ mount: '/ui', waypoint: 'index.html' })
 await bridge.ready()
 
 const runtime = new Runtime()
