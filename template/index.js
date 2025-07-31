5f57043b64808420d61ecd9cb14d62180c41d918
Geordan Gesink <165498764+geordangesink@users.noreply.github.com>
2025-07-31 22:24:38 +0200

    update template (#70)
    * bump electron and bridge; Pear.update; exit on pipe close
    
    * add update button
    
    * fix elements
    
    * fixes
    
    * bump pear-bridge
    
    * bump pear-electron
    
    ---------
    
    Co-authored-by: dmc <david.mark.clements@gmail.com>

--- template/index.js
+++ template/index.js
@@ -2,11 +2,16 @@
 import Runtime from 'pear-electron'
 import Bridge from 'pear-bridge'
 
-const bridge = new Bridge({ waypoint: '/ui/index.html' })
+Pear.updates((update) => {
+  console.log('Application update available:', update)
+})
+
+const bridge = new Bridge({ mount: '/ui', waypoint: 'index.html' })
 await bridge.ready()
 
 const runtime = new Runtime()
 const pipe = await runtime.start({ bridge })
+pipe.on('close', () => Pear.exit())
 
 pipe.on('data', (data) => {
   const cmd = Buffer.from(data).toString()
