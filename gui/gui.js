fc1e3c0b513ee5cf14feaa05530dbcee5eb528dc
Marco <maidh91@gmail.com>
2025-08-01 04:50:21 +1000

    port-fix-media-access-windows (#17)

--- gui/gui.js
+++ gui/gui.js
@@ -1789,7 +1789,7 @@ class PearGUI extends ReadyResource {
   }
 
   async askForMediaAccess ({ id, media }) {
-    if (isLinux || isWindows) return false
+    if (isLinux) return false
     if (media === 'screen') {
       return electron.systemPreferences.getMediaAccessStatus(media)
     }
