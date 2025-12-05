9dd3cf18d48a81b172b68d523d50b598ac58ba67
dmc <david.mark.clements@gmail.com>
2025-12-05 22:24:13 +0100

    dynamic asset bundling

--- scripts/prebuilds.js
+++ scripts/prebuilds.js
@@ -2,18 +2,17 @@
 const pack = require('pear-pack')
 const Localdrive = require('localdrive')
 
-async function bundle () {
+async function prebuilds () {
   const { pathname } = new URL(global.Pear.config.applink + '/')
   const drive = new Localdrive(pathname)
-  const target = ['darwin-arm64', 'darwin-x64', 'linux-arm64', 'linux-x64', 'win32-x64']
+  const hosts = ['darwin-arm64', 'darwin-x64', 'linux-arm64', 'linux-x64', 'win32-x64']
   const builtins = [
     'electron', 'net', 'assert', 'console', 'events', 'fs', 'fs/promises', 'http', 'https', 'os',
     'path', 'child_process', 'repl', 'url', 'tty', 'module', 'process', 'timers', 'inspector'
   ]
-  const { bundle, prebuilds } = await pack(drive, { target, builtins })
+  const { prebuilds } = await pack(drive, { hosts, builtins })
   for (const [prebuild, addon] of prebuilds) await drive.put(prebuild, addon)
-  await drive.put('/boot.bundle', bundle)
-  console.log('boot.bundle & prebuilds generated')
+  console.log('prebuilds generated')
 }
 
-module.exports = bundle().catch(console.error)
+module.exports = prebuilds().catch(console.error)
